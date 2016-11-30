f818bf1e2c961b530d39131a48add1ce8c14e84c
User <User@WIN-IP38677LGGL>
2016-11-30 19:14:25 +0300

    14

--- src/lesson14/Task2.java
+++ src/lesson14/Task2.java
@@ -1,10 +1,11 @@
 package lesson14;
 
-public class Task1 extends Thread {
+public class Task2 extends Thread {
 	String threadname;
 	Thread thread;
+	
 
-	Task1(String threadname) {
+	Task2(String threadname) {
 		this.threadname = threadname;
 		thread = new Thread(this, threadname);
 		System.out.println("New Thread:" + thread);
@@ -13,21 +14,24 @@ public class Task1 extends Thread {
 
 	@Override
 	public void run() {
-		
+		synchronized (this) {
 			for (int i = 0; i < 10; i++) {
 
 				System.out.print(threadname + ": " + (1 + (i * 10)) + " " + (2 + (i * 10)) + " " + (3 + (i * 10)) + " "
 						+ (4 + (i * 10)) + " " + (5 + (i * 10)) + " " + (6 + (i * 10)) + " " + (7 + (i * 10)) + " "
 						+ (8 + (i * 10)) + " " + (9 + (i * 10)) + " " + 10 * (i + 1) + '\n');
-
-			
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+				}
+			}
 		}
 	}
 
 	public static void main(String args[]) throws InterruptedException {
-		new Task1("Thread1");
+		new Task2("Thread1");
 
-		new Task1("Thread2");
+		new Task2("Thread2");
 
 	}
 }
