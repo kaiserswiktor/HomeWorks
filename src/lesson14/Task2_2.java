48bb6c3665e2e4adb5def7b7aee9d000d29488f4
User <User@WIN-IP38677LGGL>
2016-12-04 13:34:00 +0300

    2

---
+++ src/lesson14/Task2_2.java
@@ -0,0 +1,54 @@
+package lesson14;
+
+import java.util.concurrent.Semaphore;
+
+public class Task2_2 extends Thread {
+	Semaphore sem=new Semaphore(1);
+	String threadname;
+	Thread thread;
+	
+
+	Task2_2(String threadname,Semaphore sem) {
+		this.threadname = threadname;
+		this.sem=sem;
+		thread = new Thread(this, threadname);
+		System.out.println("New Thread:" + thread);
+		thread.start();
+	}
+
+	@Override
+	public synchronized void run() {
+		
+			for (int i = 0; i < 10; i++) {
+
+				System.out.print(threadname + ": " + (1 + (i * 10)) + " " + (2 + (i * 10)) + " " + (3 + (i * 10)) + " "
+						+ (4 + (i * 10)) + " " + (5 + (i * 10)) + " " + (6 + (i * 10)) + " " + (7 + (i * 10)) + " "
+						+ (8 + (i * 10)) + " " + (9 + (i * 10)) + " " + 10 * (i + 1) + '\n');
+           try {
+			sem.acquire();
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			sem.release();
+		
+			}	
+			
+	}
+
+	public static void main(String args[]) throws InterruptedException {
+		Semaphore sem=new Semaphore(1);
+		new Task2_2("Thread1", sem);
+		
+		new Task2_2("Thread2",sem);
+		
+		
+	}
+}
